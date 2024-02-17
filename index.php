59e865882b7d9cab85845133e4b46a56fbcbf38a
FairyPenguin <autorun81@gmail.com>
2024-02-17 11:25:28 +0200

    Functions & Lambda Functions

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Document</title>
-<style>
-	body{
-display:flex;
-justify-content:center;
-align-items:center;
-height:100vh;
-flex-direction:column;
-}
-</style>
-</head>
-<body>
-
-
-
-
-
-
 <?php
 
 //Array
@@ -28,82 +7,64 @@ $books = ["Book__1", "Book__2", "Book__3"];
 
 $booksData = [
     [
-        "id" => 3, "name" => "BOOK_1", "Author" => "unknown1",
+        "id" => 3, "name" => "BOOK_1", "Author" => "unknown",
     ], [
-        "id" => 4, "name" => "BOOK_2", "Author" => "unknown2",
-    ], [
-        "id" => 5, "name" => "BOOK_3", "Author" => "unknown3",
+        "id" => 4, "name" => "BOOK_2", "Author" => "unknown",
+    ],
+    [
+        "id" => 5, "name" => "BOOK_3", "Author" => "unknown2",
+    ],
+    [
+        "id" => 1, "name" => "BOOK_4", "Author" => "unknown2",
+    ],
+    [
+        "id" => 2, "name" => "BOOK_5", "Author" => "unknown3",
+    ],
+    [
+        "id" => 3, "name" => "BOOK_6", "Author" => "unknown3",
     ],
 ];
 
-// echo "<h2>PHP is Fun!</h2>";
-// echo "Hello world!<br>";
-// echo "This is just text with echo no HTM<br>";
-// echo "I'm about to learn PHP!<br>";
-// echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
-// $name = "mahmoud is here<br>";
-// echo "$name A<br>";
-// echo "$name";
-// $books = ["book1", "book2", "book3"];
+function filter($itemsArray, $fn)
+{
+    $filteredBooks = [];
 
-// echo "$books[0], $books[1], $books[2]";
-// foreach ($books as $book) {
-//     // code...
-//     echo "<li> $book </li>";
-// }
-// echo "<h1>+-+-+-+-+-+-+-+-+-+-+-+-+</h1>"
+    foreach ($itemsArray as $item) {
+        # code...
+        if ($fn($item)) {
+            # code...
+            $filteredBooks[] = $item;
+        }
 
-$name = "Laravel";
-$track = "";
+    }
 
-if ($name === "Laravel") {
-    # code...
-    $track = "Frontend";
-} else {
-    $track = "";
+    return $filteredBooks;
 }
-;
-$name;
-echo "<h1>$track $name Guide</h1>";
-echo "Hello world!<br>";
-?>
 
-<div>
-<?=$name
-// <!-- Syntax Sugar for echo -->
-?>
-</div>
+//Anonymous Function == Lambda Function
+$filterByBook = function ($booksArray, $author) {
+    $filteredBooks = [];
 
-<h3><?=$books[0]?></h3>
-<h3><?=$books[1]?></h3>
-<h3><?=$books[2]?></h3>
+    foreach ($booksArray as $book) {
+        # code...
+        if ($book["Author"] === $author) {
+            # code...
+            $filteredBooks[] = $book;
+        }
 
-<ul>
-<?php foreach ($books as $book) {
-    echo "<li>{$book} from books list</li>";
-    echo "<li>" . $book . "</li>";
-}
-;?>
-</ul>
+    }
 
-<ul>
-<?php foreach ($books as $book): ?>
+    return $filteredBooks;
+};
 
-	<li><?php echo $book ?></li>
-	<li><?=$book?></li>
+$filteredBooks = filter($booksData, function ($book) {
 
-<?php endforeach;?>
-</ul>
+    return $book["Author"] === "unknown";
 
+});
 
-<ul>
-	<?php foreach ($booksData as $book): ?>
-		<!-- <li><?=$book["id"]?></li>
-		<li><?=$book["name"]?></li>
-		<li><?=$book["Author"]?></li> -->
-		<a href="<?=$book["name"]?>"><?= $ ?></a>
-		<?php endforeach;?>
-</ul>
+$filterdBooksBuiltin = array_filter($booksData, function ($book) {
+    return $book["Author"] === "unknown";
+});
 
-</body>
-</html>
+require "./index.view.php";
---
+++ index.view.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Document</title>
+<style>
+	body{
+display:flex;
+justify-content:center;
+align-items:center;
+height:100vh;
+flex-direction:column;
+}
+</style>
+</head>
+<body>
+
+
+
+<?php
+// echo "<h2>PHP is Fun!</h2>";
+// echo "Hello world!<br>";
+// echo "This is just text with echo no HTM<br>";
+// echo "I'm about to learn PHP!<br>";
+// echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
+// $name = "mahmoud is here<br>";
+// echo "$name A<br>";
+// echo "$name";
+// $books = ["book1", "book2", "book3"];
+
+// echo "$books[0], $books[1], $books[2]";
+// foreach ($books as $book) {
+//     // code...
+//     echo "<li> $book </li>";
+// }
+// echo "<h1>+-+-+-+-+-+-+-+-+-+-+-+-+</h1>"
+
+// $name = "Laravel";
+// $track = "";
+
+// if ($name === "Laravel") {
+//     # code...
+//     $track = "Frontend";
+// } else {
+//     $track = "";
+// }
+// ;
+// $name;
+// echo "<h1>$track $name Guide</h1>";
+// echo "Hello world!<br>";
+
+/**
+ * Named Function
+ */
+?>
+
+
+
+<ul>
+<?php foreach ($booksData as $book): ?>
+
+<?php if ($book["Author"] === "unknown"): ?>
+
+
+<li><?=$book["name"]?></li>
+
+
+<?php endif?>
+
+<?php endforeach;?>
+</ul>
+
+
+
+<div>
+<?=$name
+// <!-- Syntax Sugar for echo -->
+?>
+</div>
+
+<h3><?=$books[0]?></h3>
+<h3><?=$books[1]?></h3>
+<h3><?=$books[2]?></h3>
+
+<ul>
+<?php foreach ($books as $book) {
+    echo "<li>{$book} from books list</li>";
+    echo "<li>" . $book . "</li>";
+}
+;?>
+</ul>
+
+
+
+<ul>
+<?php foreach ($books as $book): ?>
+
+	<li><?php echo $book ?></li>
+	<li><?=$book?></li>
+
+<?php endforeach;?>
+</ul>
+
+
+<ul>
+
+	<?php foreach ($filterdBooksBuiltin as $book): ?>
+
+  
+
+		<li>
+        <?=$book["id"]?>
+            <?=$book["name"]?>
+		<?=$book["Author"]?>
+		<a href="<?=$book["name"]?>"><?=$book["name"]?></a>
+    </li>
+		<?php endforeach;?>
+</ul>
+
+</body>
+</html>
